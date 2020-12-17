e9c6f0810a8ffcd72925f57a65174319fd875fbc
fsR3tR0 <fsR3tR0@github.com>
2020-12-16 22:40:40 -0600

    This is the main project, it is not done

---
+++ jarvis_prot.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python3
+
+import pyttsx3
+import ascii_art
+import webbrowser
+import smtplib 
+import random
+import speech_recognition as sr
+import wikipedia
+import datetime
+import wolframalpha
+import os
+import sys
+
+
+print('Proba proba')
+
+r = sr.Recognizer()
+# voices = engine.getProperty('voices')
+
+# webbrowser.open('www.google.com')
+# webbrowser.open_new_tab('www.youtube.com')
+
